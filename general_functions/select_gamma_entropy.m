df88be3d14019f7bde91ee29297c5024a1ed406c
Celine Brouard <celine.brouard@aalto.fi>
2017-03-01 17:28:08 +0200

    added the code for selecting gamma based on the entropy

---
+++ general_functions/select_gamma_entropy.m
@@ -0,0 +1,33 @@
+function [ gamma ] = select_gamma_entropy( X, base_kernel_type )
+%======================================================
+% DESCRIPTION:
+% Find the gamma parameter of a Gaussian kernel such that the entropy of
+% the Gaussian kernel is maximized
+%
+% INPUTS:
+% X:                matrix of size d*n containing the input or output vectors
+% base_kernel_type: type of the base kernel that will be used for computing the
+%                   pairwise distances. Typically 'linear' or 'tanimoto'
+%                   can be used, but not kernels that have parameter(s).
+%                   The Gaussian kernel is then applied on top.
+%
+% OUTPUT:
+% gamma:            Gaussian kernel parameter
+%
+%======================================================
+
+    n = size(X,2);
+    
+    % computation of the base kernel (linear or Tanimoto)
+    base_ker_opt = set_kernel_opt(base_kernel_type);
+    K = build_kernel(X, X, base_ker_opt);
+   
+    % computation of all pairwise distances
+    Kdist = diag(K)*ones(1,n) + ones(n,1)*diag(K)' - 2*K;
+
+    % the gamma parameter maximising the entropy of the Gaussian kernel is
+    % obtained by averaging all the pairwise distances and inverting this mean
+    gamma = (n*(n-1)) / (2*sum(sum(triu(Kdist,1))));
+        
+end
+

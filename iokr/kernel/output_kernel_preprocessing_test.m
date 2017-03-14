7d201a34fbd5376c5f603e2febc09fe1e36a660f
Celine Brouard <celine.brouard@aalto.fi>
2017-03-14 16:16:17 +0200

    separation of the training and test steps

--- iokr/kernel/output_kernel_preprocessing_test.m
+++ iokr/kernel/output_kernel_preprocessing_test.m
@@ -7,6 +7,5 @@ function [ KY_train_cand_cn ] = output_kernel_preprocessing_test( Y_train, Y_can
     
     KY_train_cand_cn = kernel_preprocessing_test(KY_train_cand, KY_cand, train_process, ker_center);
 
-
 end
 

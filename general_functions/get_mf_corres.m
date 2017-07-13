f93e4a2b9e4be4bac7f07e0f860e9e7bab89bf95
bach <eric.bach@aalto.fi>
2017-07-13 16:37:05 +0200

    Recover: 'get_mf_corres.m'

--- general_functions/get_mf_corres.m
+++ general_functions/get_mf_corres.m
@@ -1,1 +1,7 @@
-../iokr/general_functions/get_mf_corres.m
+function mf_corres = get_mf_corres (mfs, cand)
+    mfs_cand = arrayfun (@(x) x.set_desc, cand, 'UniformOutput', false);
+    [~, mf_corres] = ismember (mfs, mfs_cand);
+    assert (all (mf_corres > 0)); 
+end % function
+
+

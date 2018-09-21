0273e2294cb63f3a213c3e0ac7e0b922eb9a32c1
Eric Bach <eric.bach@aalto.fi>
2018-09-21 08:30:40 +0300

    Fingerprint mat is stored with hash encoding the contained spectra ids

--- general_functions/loadFingerprints.m
+++ general_functions/loadFingerprints.m
@@ -1,40 +1,47 @@
-function fps = loadFingerprints(fps_dir, spec_ids, verbose)
+function [fps, mask] = loadFingerprints(raw_fps_dir, mat_fps_dir, spec_ids, verbose)
 %% LOADFINGERPRINTS Load fingerprints from *.fpt's or *.mat file
     if nargin < 2
+        mat_fps_dir = raw_fps_dir;
+    end % if 
+    if nargin < 3
         spec_ids = {};
     end % if
-    if nargin < 3
+    if nargin < 4
         verbose = true;
     end % if
 
-    fps_mat_fn_basename = "fps.mat";
-    fps_mat_fn = strcat(fps_dir, "/", fps_mat_fn_basename);
+    % Calculate a hash value for the specified spec_ids so that the
+    % fps_HASH.mat corresponds to the desired set.
+    spec_ids_hash = string2hash(strjoin(spec_ids, '_'));
+    
+    fps_mat_fn_basename = sprintf('fps_%0.f.mat', spec_ids_hash);
+    fps_mat_fn = strcat(mat_fps_dir, "/", fps_mat_fn_basename);
     if exist(fps_mat_fn, 'file')  
         if verbose 
-            fprintf('Load "fps.mat" file.\n')
+            fprintf('Load "%s" file.\n', fps_mat_fn_basename);
         end % if
         
-        fps = load(fps_mat_fn);
-        fps = fps.fps;
+        load(fps_mat_fn);
     else
         n_spec = length(spec_ids);
         
         if verbose
-            fprintf('Load separated *.fpt files: # = %d. Create "fps.mat" subsequently.\n', n_spec);
+            fprintf('Load separated *.fpt files: # = %d. Create "%s" subsequently.\n', n_spec, ...
+                fps_mat_fn_basename);
         end % if 
         
         % Load the fingerprint mask and determine fingerprint dimension
-        fps_mask = load_fingerprint_mask(strcat(fps_dir, "/fingerprints.mask"));
-        n_fps = length(fps_mask);
+        mask = load_fingerprint_mask(strcat(raw_fps_dir, "/fingerprints.mask"));
+        n_fps = length(mask);
         
-        fps = struct('fps', sparse(n_fps, n_spec), 'mask', fps_mask);
+        fps_struct = struct('fps', false(n_fps, n_spec), 'mask', mask);
         
         for ii = 1:n_spec
             if verbose &&  ~ mod(ii, 500)
                 fprintf('Process spectra id %d.\n', ii);
             end % if
             
-            fps_spec_fn = strcat(fps_dir, "/", spec_ids{ii}, ".fpt");
+            fps_spec_fn = strcat(raw_fps_dir, "/", spec_ids{ii}, ".fpt");
             fid = fopen(fps_spec_fn, 'r');
             if fid == -1
                 error('loadFingerprints:NoSuchFileOrDirectory', 'Cannot open fps-file "%s".', ...
@@ -42,12 +49,22 @@ function fps = loadFingerprints(fps_dir, spec_ids, verbose)
             end % if
             
             % parse fingerprint vector 
-            fps.fps((fgetl(fid) - '0') == 1, ii) = true;
+            fps_struct.fps((fgetl(fid) - '0') == 1, ii) = true;
             
             fclose(fid);
         end % if
         
+        fps = fps_struct.fps;
+        mask = fps_struct.mask;
+        
         % save the fingerprint structure fur future use
-        save(fps_mat_fn, 'fps', '-v7.3');
+        save(fps_mat_fn, 'fps_struct', '-v7.3');
+    end % if
+    
+    if nargout == 1
+        fps = fps_struct;
+    elseif nargout == 2
+        fps = fps_struct.fps;
+        mask = fps_struct.mask;
     end % if
 end % function
